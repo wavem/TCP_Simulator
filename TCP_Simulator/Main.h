70416603193fdfeca6666614653700bb4e185acc
WAVE_VICTUS\fenix <fenix24@naver.com>
2024-11-26 10:25:29 +0900

    [Work] Ribbon Menu Setting

--- TCP_Simulator/Main.h
+++ TCP_Simulator/Main.h
@@ -7,10 +7,84 @@
 #include <Vcl.Controls.hpp>
 #include <Vcl.StdCtrls.hpp>
 #include <Vcl.Forms.hpp>
+#include "cxClasses.hpp"
+#include "cxControls.hpp"
+#include "cxGraphics.hpp"
+#include "cxLookAndFeelPainters.hpp"
+#include "cxLookAndFeels.hpp"
+#include "dxBar.hpp"
+#include "dxCore.h"
+#include "dxRibbon.hpp"
+#include "dxRibbonCustomizationForm.hpp"
+#include "dxRibbonSkins.hpp"
+#include "dxSkinBasic.hpp"
+#include "dxSkinBlack.hpp"
+#include "dxSkinBlue.hpp"
+#include "dxSkinBlueprint.hpp"
+#include "dxSkinCaramel.hpp"
+#include "dxSkinCoffee.hpp"
+#include "dxSkinDarkroom.hpp"
+#include "dxSkinDarkSide.hpp"
+#include "dxSkinDevExpressDarkStyle.hpp"
+#include "dxSkinDevExpressStyle.hpp"
+#include "dxSkinFoggy.hpp"
+#include "dxSkinGlassOceans.hpp"
+#include "dxSkinHighContrast.hpp"
+#include "dxSkiniMaginary.hpp"
+#include "dxSkinLilian.hpp"
+#include "dxSkinLiquidSky.hpp"
+#include "dxSkinLondonLiquidSky.hpp"
+#include "dxSkinMcSkin.hpp"
+#include "dxSkinMetropolis.hpp"
+#include "dxSkinMetropolisDark.hpp"
+#include "dxSkinMoneyTwins.hpp"
+#include "dxSkinOffice2007Black.hpp"
+#include "dxSkinOffice2007Blue.hpp"
+#include "dxSkinOffice2007Green.hpp"
+#include "dxSkinOffice2007Pink.hpp"
+#include "dxSkinOffice2007Silver.hpp"
+#include "dxSkinOffice2010Black.hpp"
+#include "dxSkinOffice2010Blue.hpp"
+#include "dxSkinOffice2010Silver.hpp"
+#include "dxSkinOffice2013DarkGray.hpp"
+#include "dxSkinOffice2013LightGray.hpp"
+#include "dxSkinOffice2013White.hpp"
+#include "dxSkinOffice2016Colorful.hpp"
+#include "dxSkinOffice2016Dark.hpp"
+#include "dxSkinOffice2019Black.hpp"
+#include "dxSkinOffice2019Colorful.hpp"
+#include "dxSkinOffice2019DarkGray.hpp"
+#include "dxSkinOffice2019White.hpp"
+#include "dxSkinPumpkin.hpp"
+#include "dxSkinsCore.hpp"
+#include "dxSkinSeven.hpp"
+#include "dxSkinSevenClassic.hpp"
+#include "dxSkinSharp.hpp"
+#include "dxSkinSharpPlus.hpp"
+#include "dxSkinSilver.hpp"
+#include "dxSkinSpringtime.hpp"
+#include "dxSkinStardust.hpp"
+#include "dxSkinSummer2008.hpp"
+#include "dxSkinTheAsphaltWorld.hpp"
+#include "dxSkinTheBezier.hpp"
+#include "dxSkinValentine.hpp"
+#include "dxSkinVisualStudio2013Blue.hpp"
+#include "dxSkinVisualStudio2013Dark.hpp"
+#include "dxSkinVisualStudio2013Light.hpp"
+#include "dxSkinVS2010.hpp"
+#include "dxSkinWhiteprint.hpp"
+#include "dxSkinWXI.hpp"
+#include "dxSkinXmas2008Blue.hpp"
 //---------------------------------------------------------------------------
 class TFormMain : public TForm
 {
 __published:	// IDE-managed Components
+	TdxRibbonTab *dxRibbon1Tab1;
+	TdxRibbon *dxRibbon1;
+	TdxBarManager *BarMgr;
+	TdxBar *BarMgrBar1;
+	TdxBarLargeButton *dxBarLargeButton1;
+	TdxBarLargeButton *dxBarLargeButton2;
 private:	// User declarations
 public:		// User declarations
 	__fastcall TFormMain(TComponent* Owner);
